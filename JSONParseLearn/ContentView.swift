9a82d11ca706d7b8a41bef2249bc0b118355aa6d
Peter Hartnett <peter.hartnett@gmail.com>
2022-02-24 11:43:18 -0500

    Initial Commit

---
+++ JSONParseLearn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONParseLearn
+//
+//  Created by Peter Hartnett on 2/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSONParseLearn/JSONParseLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParseLearnApp.swift
+//  JSONParseLearn
+//
+//  Created by Peter Hartnett on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct JSONParseLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
